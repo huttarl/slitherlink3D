b218e39f8913ec282f83aa5589d647043567904b
Lars Huttar <lars@100fold.org>
2025-10-20 17:22:27 -0400

    Maintain dual graph to check face connectivity. Normalize vertices. Fix "boring" areas.

--- util/genSliPuzzles.py
+++ util/genSliPuzzles.py
@@ -3,13 +3,14 @@ Usage: python3 genSliPuzzles.py myGrid.json
 Output is written to stdout.
 For JSON format specifications, see docs/json-format.md."""
 
-import json, sys
-import time, random
+import json, random, sys, math
+from random import choice
 
-from compas.datastructures import Mesh
-import networkx as nx
 import matplotlib.pyplot as plt
-from matplotlib.figure import Figure, FigureBase
+import networkx as nx
+from compas.datastructures import Mesh
+from compas.geometry import centroid_points, Point, length_vector
+from matplotlib.figure import Figure
 from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
@@ -40,6 +41,7 @@ total_red = 0
 total_blue = 0
 red_needs_check = False
 blue_needs_check = False
+opposite_color = {"red": "blue", "blue": "red"}
 
 def require_properties(properties):
     """Ensure that all required properties are present in the grid JSON."""
@@ -63,7 +65,7 @@ def setup_display():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     fig.canvas.mpl_connect('key_press_event', on_key_press)
-    plt.ion() # enable interactive mode
+    # plt.ion() # enable interactive mode
 
 
 def update_display():
@@ -74,12 +76,23 @@ def update_display():
     ax.clear()
     # TODO: since the V/E/F don't change, only their colors, it would be better
     # not to rebuild the mesh, but just update the colors.
+    # Build the Poly3DCollection and its labels once.
     faces = [[mesh.vertex_coordinates(vkey) for vkey in mesh.face_vertices(fkey)]
              for fkey in mesh.faces()]
     colors = [mesh.face_attribute(fkey, 'color') for fkey in mesh.faces()]
 
     poly = Poly3DCollection(faces, facecolors=colors, edgecolor='gray', alpha=0.8, linewidths=2)
     ax.add_collection3d(poly)
+
+    # Label each face at its centroid
+    for fkey in mesh.faces():
+        pts = [mesh.vertex_coordinates(vkey) for vkey in mesh.face_vertices(fkey)]
+        cx, cy, cz = centroid_points(pts)
+        # Move these away from origin.
+        factor = 1.15
+        ax.text(cx * factor, cy * factor, cz * factor, str(fkey),
+                color='black', fontsize=8, ha='center', va='center')
+
     xs, ys, zs = zip(*[mesh.vertex_coordinates(v) for v in mesh.vertices()])
     ax.auto_scale_xyz(xs, ys, zs)
     ax.set_box_aspect([1, 1, 1])
@@ -89,8 +102,20 @@ def update_display():
     plt.axis('off')
 
     plt.draw()
-    print("Displaying mesh...")
+    # print("Displaying mesh...")
     plt.pause(0.01)  # brief pause to refresh display
+
+
+def log_mesh():
+    """Log faces, edges of built mesh for debugging."""
+    for (fkey) in mesh.faces():
+        edges = ", ".join(str(ekey) for ekey in mesh.face_halfedges(fkey))
+        print(f"Face {fkey}: {edges}")
+    for (ekey) in mesh.edges():
+        # Which two faces are connected by this edge?
+        (f1, f2) = mesh.edge_faces(ekey)
+        print(f"Edge {ekey}: f{f1} <-> f{f2}")
+
 
 
 def build_graphs():
@@ -101,6 +126,9 @@ def build_graphs():
     mesh = Mesh.from_vertices_and_faces(grid_vertices, grid_faces)
     # That was easy!
     print(f"Built mesh. F: {mesh.number_of_faces()}, V: {mesh.number_of_vertices()}, E: {mesh.number_of_edges()}")
+    log_mesh()
+    normalize_vertices()
+
     # Now make a dual graph in nx, with nodes for the faces of the grid.
     # This will be used for connectedness queries.
     dualG = nx.Graph()
@@ -117,6 +145,28 @@ def build_graphs():
     #     print(vertex)
 
 
+def normalize_vertices():
+    """Adjust vertices to be centered about the origin, and about 1 unit away."""
+    # Compute average vertex position.
+    vertex_position_total = Point(0, 0, 0)
+    for v in mesh.vertices():
+        vertex_position_total += mesh.vertex_point(v)
+    avg_vertex_position = vertex_position_total / num_vertices
+    # print(f"Average vertex position before normalizing: {avg_vertex_position}")
+
+    # Adjust displacement, and compute distance.
+    max_distance = 0
+    for v in mesh.vertices():
+        p = mesh.vertex_point(v) - avg_vertex_position
+        mesh.set_vertex_point(v, p)
+        # Squared distance from origin.
+        max_distance = max(max_distance, length_vector(p))
+    print(f"Max distance from origin before normalizing: {max_distance}")
+
+    for v in mesh.vertices():
+        mesh.set_vertex_point(v, mesh.vertex_point(v) / max_distance)
+
+
 def process_grid_json():
     """Validate the grid JSON data and put into efficient data structures."""
     global grid_json, grid_id, grid_faces, num_faces, grid_vertices, num_vertices, puzzles_output
@@ -128,7 +178,7 @@ def process_grid_json():
 
     grid_vertices = grid_json["vertices"]
     num_vertices = len(grid_json["vertices"])
-    # We're not actually going to need any vertex positions. Just their indices.
+    # Vertex positions will be used only for debugging display.
 
     grid_faces = grid_json["faces"]
     num_faces = len(grid_faces)
@@ -177,14 +227,21 @@ def paint_random_faces(color, how_many):
         while True:
             fkey = random.choice(list(mesh.faces()))
             if mesh.face_attribute(fkey, "color") != color:
-                mesh.face_attribute(fkey, "color", color)
-                dualG.nodes[fkey]["color"] = color
+                paint_face(fkey, color)
                 break # out of 'while', continue 'for'
+
+
+def paint_face(fkey, color):
+    """Paint the given face the given color.
+    Adjusts totals, and updates dual graph and *_needs_check as needed."""
+    global total_red, total_blue, red_needs_check, blue_needs_check
+    mesh.face_attribute(fkey, "color", color)
+    dualG.nodes[fkey]["color"] = color
     if color == "red":
-        total_red += how_many
+        total_red += 1
         blue_needs_check = True
     else:
-        total_blue += how_many
+        total_blue += 1
         red_needs_check = True
 
 
@@ -199,28 +256,65 @@ def adjust_populations():
 def ensure_connected(color):
     """Check whether faces of the given color are connected.
     If not, add paint until they are.
-    Return True if they were already connected, False if we had to paint any faces."""
+    Return True if any faces were painted, False if the faces were already connected."""
     print(f"Ensuring connectedness of faces of color {color}.")
-    failed = False
+    faces_painted = False
     while True:
         # Collect face nodes of the given color.
         p = dualG.nodes(data=True)
-        print(f"Dual graph has {len(p)} nodes: {repr(p)}")
+        # print(f"Dual graph has {len(p)} nodes") # {repr(p)}
         this_color_face_nodes = [f for f, d in dualG.nodes(data=True) if d['color'] == color]
         is_connected = nx.is_connected(dualG.subgraph(this_color_face_nodes))
         print(f"Connectedness of {len(this_color_face_nodes)} {color}: {is_connected}.")
         update_display()
 
         if is_connected:
-            return not failed
-        else:
-            failed = True
+            return faces_painted
+
         # At this point I had thought to pick a face adjacent to one of the connected groups.
         # But it may be just as effective (and is easier) to just paint a random face.
         paint_random_faces(color, 1)
+        faces_painted = True
         # TODO: re-collecting color face nodes in order to re-check connectedness is inefficient.
         # Instead, add the just-painted face to the face node collection.
         update_display()
+
+
+def fix_boring_neighborhoods():
+    """Disrupt neighborhoods of where faces are all the same color."""
+    # Set all faces to "boring".
+    for fkey in mesh.faces():
+        mesh.face_attribute(fkey, "boring", True)
+    for ekey in mesh.edges():
+        # For every edge, get the two faces it connects.
+        (f1, f2) = mesh.edge_faces(ekey)
+        print(f"Checking edge {ekey} (f{f1}, f{f2})...")
+        if (mesh.face_attribute(f1, "color") != mesh.face_attribute(f2, "color")):
+            print(f"Edge {ekey} has different colors on faces {f1} and {f2}.")
+            # Faces that have different-colored neighbors are not "boring".
+            mesh.face_attribute(f1, "boring", False)
+            mesh.face_attribute(f2, "boring", False)
+
+    # Now check for boring faces with all-boring neighbors.
+    num_boring_faces = 0
+    for fkey in mesh.faces():
+        if mesh.face_attribute(fkey, "boring"):
+            print(f"Boring face {fkey} is {mesh.face_attribute(fkey, 'color')}.")
+            num_boring_faces += 1
+            # Check if any of the neighbors are also boring.
+            for nbr in mesh.face_neighbors(fkey):
+                if mesh.face_attribute(nbr, "boring"):
+                    # We have two adjacent boring faces.
+                    print(f"Boring face {fkey} has a boring neighbor {nbr}.")
+                    # Paint one of them the opposite color.
+                    f_to_color = random.choice([fkey, nbr])
+                    old_color = mesh.face_attribute(f_to_color, "color")
+                    print(f"  Painting face {f_to_color} {opposite_color[old_color]}")
+                    paint_face(f_to_color, opposite_color[old_color])
+                    # Stop processing this face. Check other boring faces (continue outer loop).
+                    break
+
+
 
 
 def generate_puzzle(i):
@@ -228,25 +322,31 @@ def generate_puzzle(i):
     global total_red, total_blue, blue_needs_check, red_needs_check
     randomize_face_colors()
     print(f"Generated puzzle {i+1} with {total_red} red faces and {total_blue} blue faces.")
+    update_display()
     finished = False
     blue_needs_check = True
     red_needs_check = True
     while not finished:
-        adjust_populations()
+        adjust_populations() # Could trigger red_needs_check or blue_needs_check.
         if blue_needs_check:
-            while not ensure_connected('blue'):
+            # Make sure blue is connected.
+            added_blue = ensure_connected('blue')
+            blue_needs_check = False
+            # If that required painting faces blue...
+            if added_blue:
                 red_needs_check = True
         if red_needs_check:
-            while not ensure_connected('red'):
+            # Make sure red is connected.
+            added_red = ensure_connected('red')
+            red_needs_check = False
+            # If that required painting faces red...
+            if added_red:
                 blue_needs_check = True
+        fix_boring_neighborhoods()
         finished = not(blue_needs_check or red_needs_check)
+        print(f"blue needs check: {blue_needs_check} red needs check: {red_needs_check}")
 
-    # Display...
-    for step in range(10):
-        # ... modify your mesh here (e.g., move vertices, change topology) ...
-        update_display()
-        time.sleep(0.2)  # simulate computation time
-
+    print("Achieved acceptable red and blue connected regions!")
     plt.show()
 
 def randomize_face_colors():
